56a3bbd1a8610a4f70aca3f815f6bae11ffd7e81
Ian <ian.wanjira@student.moringaschool.com>
2021-12-07 21:19:43 +0300

    added path to urls.py file in project folder

--- insta_clone/views.py
+++ insta_clone/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def register(request):
-    return render(request,'auth/registration.html', {"form":form})
+    return render(request,'auth/registration.html')
